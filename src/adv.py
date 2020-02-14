04b6468bc15457d792d867a0d34b72402261b845
gebhartn <nicholas.gebhart@gmail.com>
2020-02-13 16:56:32 -0800

    feat: controller

--- src/adv.py
+++ src/adv.py
@@ -1,26 +1,30 @@
-from room import Room
 import sys
+from room import Room
 from player import Player
+from item import Item
 
 # Declare all the ROOMs
 
+LONG_SWORD = Item('sword', 'stick them with the pointy end')
+ROCK = Item('rock', 'this could be useful, I suppose...')
+
 ROOM = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", [LONG_SWORD, ROCK]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", [ROCK]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", []),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", []),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", []),
 }
 
 
@@ -35,26 +39,29 @@ ROOM['narrow'].w_to = ROOM['foyer']
 ROOM['narrow'].n_to = ROOM['treasure']
 ROOM['treasure'].s_to = ROOM['narrow']
 
-#
-# Main
-#
+# Output
+WARNING = """
+=============== WARNING ===============
+==    That is not a real command!    ==
+==         Press h for help          ==
+=======================================
+"""
 
-
-def input_options():
-    """
-    Prints game opions
-    """
-
-    print("""
+OPTIONS = """
 =============== HELP ME ===============
 ==      Use [n, s, e ,w ] to move    ==
+== Use [yoink, show, drop] for items ==
 ==         Press q to quit           ==
 ==      Press h for this menu        ==
 =======================================
-    """)
+"""
 
+VIABLE = ["n", "s", "e", "w", "h", "q", "yoink", "show", "drop"]
+DIRECTIONS = ["n", "s", "e", "w"]
 
-VIABLE = ["n", "s", "e", "w", "h", "q"]
+#
+# Main
+#
 
 
 def main():
@@ -63,27 +70,34 @@ def main():
     """
 
     # Make a new player object that is currently in the 'outside' ROOM.
-    player = Player("Nicholas", ROOM['outside'])
+    player = Player(input('What is your name? '), ROOM['outside'], [])
 # Write a loop that:
 
     while True:
+        print("")
         print(player.current_room.name)
         print(player.current_room.description)
-        result = input("Where would you like to go? [n/s/e/w] ").lower()
+        print(player.current_room.list_items())
+        result = input(
+            "What would you like to do? ").lower().split()
 
-        if result in VIABLE:
-            if result == "h":
-                input_options()
-            if result == "q":
-                print('\nGoodbye!\n')
+        if result[0] in VIABLE:
+            if result[0] in DIRECTIONS:
+                player.travel(result[0])
+            if result[0] == "yoink":
+                player.pickup(result[1])
+            if result[0] == "drop":
+                player.drop(result[1])
+            if result[0] == "show":
+                player.list_inventory()
+            if result[0] == "h":
+                print(OPTIONS)
+            if result[0] == "q":
+                print(
+                    f"\nQuitting Adventure, {player.name}. You'll a coward!\n")
                 sys.exit()
         else:
-            print("""
-=============== WARNING ===============
-==    That is not a real command!    ==
-==         Press h for help          ==
-=======================================
-""")
+            print(WARNING)
 
 
 # * Prints the current ROOM name
